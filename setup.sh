0658d892ac34469489c1b7a385dfd417c6cb0f7f
Chris McKnight <cmckni3@gmail.com>
2014-06-25 03:03:31 -0500

    Fix symlinks

--- setup.sh
+++ setup.sh
@@ -1,25 +1,38 @@
 set -e
 
+DOT_FILES=$HOME/dotfiles
+ZSH=$(which zsh)
+
+# Check for prerequisites
+if [[ -z "$ZSH" ]]; then
+  echo 'Please install zsh'
+  exit
+fi
+
 # Global ignore for git and mercurial
-ln -s gitignore_global ~/.gitignore_global
-ln -s hgignore_global ~/.hgignore_global
+ln -s $DOT_FILES/gitignore_global ~/.gitignore_global
+ln -s $DOT_FILES/hgignore_global ~/.hgignore_global
 
 # Ruby
-ln -s gemrc ~/.gemrc
-ln -s irbrc ~/.irbrc
-ln -s rbenv ~/.rbenv
+ln -s $DOT_FILES/gemrc ~/.gemrc
+ln -s $DOT_FILES/irbrc ~/.irbrc
+ln -s $DOT_FILES/.rbenv ~/.rbenv
 
-ln -s nvm ~/.nvm
+ln -s $DOT_FILES/.nvm ~/.nvm
 
 # oh my zsh
-ln -s oh-my-zsh ~/.oh-my-zsh
-ln -s zshrc ~/.zshrc
+ln -s $DOT_FILES/.oh-my-zsh ~/.oh-my-zsh
+ln -s $DOT_FILES/zshrc ~/.zshrc
 
 # tmux
-ln -s tmux.conf ~/.tmux.conf
+ln -s $DOT_FILES/tmux.conf ~/.tmux.conf
 
 # vim
-ln -s vimrc ~/.vimrc
+ln -s $DOT_FILES/vimrc ~/.vimrc
+
+chsh -s $ZSH
+zsh
+source $HOME/.zshrc
 
 # Install ruby
 rbenv install 1.9.3-p545
@@ -27,12 +40,15 @@ rbenv install 2.0.0-p481
 
 rbenv global 1.9.3-p545
 
-# Install homebrew
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # Install homebrew
+  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+
+  # Sublime Text 3 preferences
+  mkdir -p "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+  ln -s $DOT_FILES/Preferences.sublime-settings "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+fi
 
 # Install nodejs
 nvm install 0.10
 nvm alias default 0.10
-
-mkdir -p "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
-ln -s Preferences.sublime-settings "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
