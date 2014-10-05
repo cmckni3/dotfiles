38e43a00c5412b4b9b32bbd1dc1d88e64acbfb25
Chris McKnight <cmckni3@gmail.com>
2014-10-05 01:55:10 -0500

    Update setup file

--- setup.sh
+++ setup.sh
@@ -1,13 +1,8 @@
 set -e
 
 DOT_FILES=$HOME/dotfiles
-ZSH=$(which zsh)
 
-# Check for prerequisites
-if [[ -z "$ZSH" ]]; then
-  echo 'Please install zsh'
-  exit
-fi
+echo 'Linking configuration files'
 
 # Global ignore for git and mercurial
 ln -s $DOT_FILES/gitignore_global ~/.gitignore_global
@@ -17,30 +12,40 @@ ln -s $DOT_FILES/hgignore_global ~/.hgignore_global
 ln -s $DOT_FILES/gemrc ~/.gemrc
 ln -s $DOT_FILES/irbrc ~/.irbrc
 
-# oh my zsh
-ln -s $DOT_FILES/zshrc ~/.zshrc
-
 # tmux
 ln -s $DOT_FILES/tmux.conf ~/.tmux.conf
 
 # vim
 ln -s $DOT_FILES/vimrc ~/.vimrc
 
+ZSH=$(which zsh)
+
+# Check for prerequisites
+if [ -z "$ZSH" ]; then
+  echo 'Please install zsh'
+  exit
+fi
+
+echo 'Linking zshrc'
+ln -s $DOT_FILES/zshrc ~/.zshrc
+
 echo " Changing shell...."
-sudo echo "/usr/local/bin/zsh" >> /etc/shells && chsh -s /usr/local/bin/zsh
-chsh -s $ZSH
+sudo echo "/usr/local/bin/zsh" >> /etc/shells && chsh -s $ZSH
 zsh
 source $HOME/.zshrc
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # Install homebrew
+  echo 'Installing homebrew'
   ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
+  echo 'Installing homebrew formulas'
+  sh $DOT_FILES/homebrew.sh
+
   # Sublime Text 3 preferences
+  echo 'Linking Sublime Text 3 Preferences'
   mkdir -p "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
   ln -s $DOT_FILES/Preferences.sublime-settings "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
-
-  sh $DOT_FILES/homebrew.sh
 fi
 
 # Install ruby
