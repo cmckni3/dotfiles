7704d7c7d9c8de9b64785f33dedb902b14515902
Chris McKnight <cmckni3@gmail.com>
2014-10-13 02:38:30 -0500

    Update homebrew formulas

--- homebrew/formula.sh
+++ homebrew/formula.sh
@@ -17,36 +17,68 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   FORMULAS=(
     ack
     apple-gcc42
+    atlassian-plugin-sdk
+    bats
     boot2docker
     brew-cask
-    caskroom/cask/brew-cask
+    composer
     coreutils
+    ctags
+    curl
+    dart
+    dnsmasq
     dock
+    elasticsearch
+    ghostscript
+    git
     git-flow
     git-extras
+    gnu-sed
+    gnu-tar
+    go
+    graphviz
     htop-osx
     hub
+    imagemagick
+    memcached
     mercurial
+    mit-scheme
     mogenerator
     mongodb
     mysql
+    neo4j
     nginx
+    nmap
+    objc-run
     openssl
     phantomjs
     php53 php53-mcrypt php53-memcached php53-pcntl php53-redis php53-xdebug php53-yaml
+    phplint
+    pianobar
     postgresql
     python
+    python3
+    qt
+    r
+    ragel
     rbenv
     rbenv-binstubs
     rbenv-ctags
     rbenv-default-gems
     rbenv-gem-rehash
     rbenv-gemset
+    redis
     ruby-build
     redis
     sqlite
+    swaks
+    the_silver_searcher
     tmux
+    tree
+    valgrind
     wget
+    wp-cli
+    xctool
     zsh
   )
 
