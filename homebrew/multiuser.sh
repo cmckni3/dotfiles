7f21362decb116e96ab34174eb540aab4f26b37d
Chris McKnight <cmckni3@gmail.com>
2014-10-19 22:26:10 -0500

    Adds multiuser homebrew setup script

---
+++ homebrew/multiuser.sh
@@ -0,0 +1,14 @@
+let n=`sudo dscl . -list /Groups PrimaryGroupID | awk '{print $NF}' | sort -n | tail -1`
+
+GROUP_ID=$(( n += 1 ))
+GROUP_NAME=brew
+
+sudo dscl . create /Groups/$GROUP_NAME
+sudo dscl . create /Groups/$GROUP_NAME PrimaryGroupID $GROUP_ID
+sudo dscl . create /Groups/$GROUP_NAME RealName "Homebrew Users"
+sudo dscl . create /Groups/$GROUP_NAME GroupMembership `whoami`
+
+sudo chgrp -R brew /usr/local
+sudo chmod -R g+w /usr/local
+sudo chgrp -R brew /Library/Caches/Homebrew
+sudo chmod -R g+w /Library/Caches/Homebrew
