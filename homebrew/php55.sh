750187f9798fb6f553304699d41e8fc43df54f42
Chris McKnight <cmckni3@gmail.com>
2015-02-09 12:05:07 -0600

    Create php55.sh

---
+++ homebrew/php55.sh
@@ -0,0 +1,2 @@
+brew install php55 --with-fpm --without-snmp --with-mssql --with-postgresql --with-mysql --with-homebrew-openssl
+brew install php55-mcrypt php55-memcached php55-pcntl php55-redis php55-xdebug php55-yaml
