bceb9de10f391cb26d845e638871da3dfbfcdc58
Chris McKnight <cmckni3@gmail.com>
2015-02-09 12:05:57 -0600

    Create php54.sh

---
+++ homebrew/php54.sh
@@ -0,0 +1,2 @@
+brew install php54 --with-fpm --without-snmp --with-mssql --with-postgresql --with-mysql --with-homebrew-openssl
+brew install php54-mcrypt php54-memcached php54-pcntl php54-redis php54-xdebug php54-yaml
