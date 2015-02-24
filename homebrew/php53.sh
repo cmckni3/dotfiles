b27ebf15e342801bbc8d021382976e6fef459dcb
Chris McKnight <cmckni3@gmail.com>
2015-02-24 01:01:51 -0600

    Delete php53.sh

--- homebrew/php53.sh
+++
@@ -1,2 +0,0 @@
-brew install php53 --with-fpm --without-snmp --with-mssql --with-postgresql --with-mysql --with-homebrew-openssl
-brew install php53-mcrypt php53-memcached php53-pcntl php53-redis php53-xdebug php53-yaml
