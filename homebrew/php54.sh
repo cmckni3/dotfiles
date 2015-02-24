9ba83cf2afffe63853331f12a61ecbe17502b28c
Chris McKnight <cmckni3@gmail.com>
2015-02-24 01:01:56 -0600

    Delete php54.sh

--- homebrew/php54.sh
+++
@@ -1,2 +0,0 @@
-brew install php54 --with-fpm --without-snmp --with-mssql --with-postgresql --with-mysql --with-homebrew-openssl
-brew install php54-mcrypt php54-memcached php54-pcntl php54-redis php54-xdebug php54-yaml
