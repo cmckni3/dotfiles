9d7fd74733968c07d132254125d91de49953fe50
Chris McKnight <cmckni3@gmail.com>
2015-04-17 18:25:10 -0500

    Update php55-dev.sh

--- homebrew/php55-dev.sh
+++ homebrew/php55-dev.sh
@@ -1,4 +1,8 @@
 # Install PHP 5.5 for extension development
 brew install php55 --with-thread-safety --with-debug --with-phpdbg --with-fpm --without-snmp --with-mssql --with-postgresql --with-mysql --with-homebrew-openssl
 brew install php55-mcrypt php55-pcntl php55-yaml
-brew install php55-redis php55-xdebug php55-xhprof --HEAD
+brew install php55-redis php55-xdebug --HEAD
+
+# Profiling
+brew install php55-mongo php55-xhprof --HEAD
+brew install php55-xhgui
