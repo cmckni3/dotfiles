eed0a24f1eda76feaa421037fe6838db2210dfcd
Chris McKnight <cmckni3@gmail.com>
2015-08-08 00:07:50 -0500

    Removes php55-pcntl

--- homebrew/php55.sh
+++ homebrew/php55.sh
@@ -3,6 +3,6 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
 
   brew install php55 --with-fpm --without-snmp --with-mssql --with-postgresql --with-mysql --with-homebrew-openssl --with-imap
-  brew install php55-mcrypt php55-memcached php55-pcntl php55-redis php55-xdebug php55-yaml
+  brew install php55-mcrypt php55-memcached php55-redis php55-xdebug php55-yaml
 
 fi
