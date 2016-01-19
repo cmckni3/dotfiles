f64c4aeeeed311131c6c3c7af41fb904047276a7
Chris McKnight <cmckni3@gmail.com>
2016-01-18 22:29:47 -0600

    Update php56.sh

--- homebrew/php56.sh
+++ homebrew/php56.sh
@@ -3,6 +3,8 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
 
   brew install php56 --with-fpm --without-snmp --with-mssql --with-postgresql --with-mysql --with-homebrew-openssl --with-imap
-  brew install php56-mcrypt php56-memcached php56-redis php56-xdebug php56-yaml
+  brew install php56-mcrypt php56-yaml
+  brew install php56-xdebug
+  brew install php56-memcached php56-redis
 
 fi
