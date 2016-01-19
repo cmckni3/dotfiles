72cbf6519d6c0d8634a56cafe17d570ac78e15a6
Chris McKnight <cmckni3@gmail.com>
2016-01-18 22:30:17 -0600

    Update php55.sh

--- homebrew/php55.sh
+++ homebrew/php55.sh
@@ -3,6 +3,8 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
 
   brew install php55 --with-fpm --without-snmp --with-mssql --with-postgresql --with-mysql --with-homebrew-openssl --with-imap
-  brew install php55-mcrypt php55-memcached php55-redis php55-xdebug php55-yaml
+  brew install php55-mcrypt php55-yaml
+  brew install php55-xdebug
+  brew install php55-memcached php55-redis
 
 fi
