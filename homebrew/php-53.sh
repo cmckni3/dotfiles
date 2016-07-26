8628551596d8c032ad23fbf95ca0fa721dd9cbe2
Chris McKnight <cmckni3@gmail.com>
2016-07-26 14:11:21 -0500

    Create php-53.sh

---
+++ homebrew/php-53.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+
+  brew install php53 --with-fpm --without-snmp --with-mssql --with-postgresql --with-mysql --with-homebrew-openssl --with-imap
+  brew install php53-mcrypt php53-yaml
+  brew install php53-xdebug
+  brew install php53-memcached php53-redis
+
+fi
