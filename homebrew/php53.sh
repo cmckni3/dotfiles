d8bca22489689fd1ec63f2e46b0f068a98a1b3e5
Chris McKnight <cmckni3@gmail.com>
2017-02-02 09:39:21 -0600

    chore(php53): Update php53 install command

--- homebrew/php53.sh
+++ homebrew/php53.sh
@@ -2,7 +2,7 @@
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
 
-  brew install php53 --with-fpm --without-snmp --with-mssql --with-postgresql --with-mysql --with-homebrew-openssl --with-imap
+  brew install php53 --with-httpd24 --without-snmp --with-mssql --with-postgresql --with-mysql --with-homebrew-openssl --with-imap
   brew install php53-mcrypt php53-yaml
   brew install php53-xdebug
   brew install php53-memcached php53-redis
