5ce18e8faa0bd79c694a02a85fa3d78281dc63ef
Chris McKnight <cmckni3@gmail.com>
2017-02-28 13:30:37 -0600

    chore(sublime): Add sublime keymap and snippets

--- sublime/install.sh
+++ sublime/install.sh
@@ -6,11 +6,13 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   SUBLIME_PACKAGE_DIR="$HOME/Library/Application Support/Sublime Text 3/Packages"
   USER_PACKAGES_DIR="$SUBLIME_PACKAGE_DIR/User"
   echo 'Linking Sublime Text 3 Preferences'
-  mkdir -p "$USER_PACKAGES_DIR"
+  mkdir -p "$USER_PACKAGES_DIR/snippets"
   ln -nfs "$DIR/Preferences.sublime-settings" "$USER_PACKAGES_DIR"
   ln -nfs "$DIR/Package Control.sublime-settings" "$USER_PACKAGES_DIR"
   ln -nfs "$DIR/SublimeLinter.sublime-settings" "$USER_PACKAGES_DIR"
   ln -nfs "$DIR/Git.sublime-settings" "$USER_PACKAGES_DIR"
+  ln -nfs "$DIR/Default (OSX).sublime-keymap" "$USER_PACKAGES_DIR"
+  ln -nfs "$DIR/snippets/phpunit.sublime-snippet" "$USER_PACKAGES_DIR/snippets"
   # Fixes Seti theme issues after updating to 10.0...
   # See https://github.com/ctf0/Seti_ST3/issues/207
   SETI_DIR="$USER_PACKAGES_DIR/Seti_UI/Scheme"
