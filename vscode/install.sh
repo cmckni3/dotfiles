e6cf1a69fd211987df2c3a7687a9f7d26be8dc82
Chris McKnight <cmckni3@gmail.com>
2017-09-05 14:37:38 -0500

    chore(vscode): Add vscode settings

---
+++ vscode/install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # Visual Studio Code preferences
+  DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+  VSCODE_PACKAGE_DIR="$HOME/Library/Application Support/Code"
+  USER_PACKAGES_DIR="$VSCODE_PACKAGE_DIR/User"
+  echo 'Linking Visual Studio Code Preferences'
+  mkdir -p "$USER_PACKAGES_DIR/snippets"
+  ln -nfs "$DIR/settings.json" "$USER_PACKAGES_DIR"
+
+  CODE_INSTALLED=true
+  type code >/dev/null 2>&1 || { echo 'Visual Studio Code not installed. Skip installing extensions'; CODE_INSTALLED=false; }
+
+  if $CODE_INSTALLED; then
+    echo 'here'
+    exts=$(cat "$DIR/extensions.txt")
+    echo $exts
+    for ext in $exts; do
+      echo "Installing $ext"
+      code --install-extension "$ext"
+    done
+  fi
+fi
