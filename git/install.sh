f6c1649fe213453da9fd80d40b60896cab5dd117
Chris McKnight <cmckni3@gmail.com>
2017-10-25 02:18:02 -0500

    chore(git): Add commit message template

--- git/install.sh
+++ git/install.sh
@@ -9,3 +9,4 @@ ln -nfs "$DIR/gitignore_global" ~/.gitignore_global
 ln -nfs "$DIR/gitconfig" ~/.gitconfig
 ln -nfs "$DIR/gitconfig.local" ~/.gitconfig.local
 ln -nfs "$DIR/gitconfig.shared" ~/.gitconfig.shared
+ln -nfs "$DIR/gitmessage" ~/.gitmessage
