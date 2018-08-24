5e49bf970bd48bf98c46a4ccdfeb8eaaf675fd8a
Chris McKnight <cmckni3@gmail.com>
2018-08-24 08:04:34 -0500

    chore(mac): Adjust key repeat values

--- mac/defaults.sh
+++ mac/defaults.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env sh
 
+# I wanna go fast
+defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
+
+defaults write com.apple.finder DisableAllAnimations -bool true
+
+defaults write com.apple.dock launchanim -bool false
+
+defaults write com.apple.dock expose-animation-duration -float 0.1
+
+defaults write com.apple.dock autohide-delay -float 0
+
+defaults write com.apple.mail DisableReplyAnimations -bool true
+defaults write com.apple.mail DisableSendAnimations -bool true
+
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+defaults write NSGlobalDomain KeyRepeat -int 6
+
 # Disable .DS_Store on network drives
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
