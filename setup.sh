9df219f4e0203145c0b3ff16d5d330fd2eef37f1
Chris McKnight <cmckni3@gmail.com>
2019-02-28 11:57:18 -0600

    fix(setup): Update nvm

--- setup.sh
+++ setup.sh
@@ -78,7 +78,7 @@ rbenv global 2.3.4
 
 # Install nodejs
 echo 'Installing nvm'
-curl https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+curl https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 echo 'Installing node.js'
 nvm install 8 && nvm install 10
 nvm alias default 8
