ca47d26c302eb95864c6655473f00398b98e03b3
Chris McKnight <cmckni3@gmail.com>
2022-09-30 11:37:11 -0500

    fix(setup): Use new homebrew sh script

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -2,7 +2,7 @@
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   echo 'Installing homebrew'
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   brew install hub
 fi
 
